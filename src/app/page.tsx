35fae26a7efc1abc170f92ac5b7da74625860353
oneshi <s.yamane@newbees.jp>
2024-08-08 11:31:18 +0900

    ファーストコミット
    国データ検索ページを作成しました。
    
    ## 課題元
    
    https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import styles from "@/css/page.module.css";
+import { Search } from "@/components/Search";
+import { RegionSelect } from "@/components/RegionSelect";
+import { CountryCard } from "@/components/CountryCard";
+import { useState } from "react";
+import { useCountryData } from "@/context/CountryDataContext"; //カスタムフック使ってAPIデータ使うよ
 
 export default function Home() {
+  //RegionSelectのための状態保管
+  const [selectedRegion, setSelectedRegion] = useState<string>('all');
+  //Searchのための状態保管
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  //CountryDataContextで作ったカスタムフックを使用
+  const { countryData, loading, error } = useCountryData();
+  //RegionSelect
+  const handleRegionChange = (region: string) => {
+    setSelectedRegion(region);
+  };
+  
+  //検索などによるフィルタリング
+  const filteredCountries = countryData.filter(country => 
+    (selectedRegion === 'all' || country.region === selectedRegion) &&
+    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  if (loading) { return <div>Loading...</div>; }
+  if (error) { return <div>Error: {error}</div>; }
+
+  //Search
+  const handleSearchChange = (query: string) => {
+    setSearchQuery(query);
+  };
+
   return (
     <main className={styles.main}>
-
+      <div className={styles.searchSelectBox}>
+        <Search onSearchSubmit={handleSearchChange} />
+        <RegionSelect onRegionChange={handleRegionChange} />
+      </div>
+      <CountryCard data={filteredCountries} />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "@/css/page.module.css";
+
+export const Footer = () => {
+	return(
+		<footer className={styles.footer}>
+			<p>FE Challenge 2024</p>
+		</footer>
+	)
+}
